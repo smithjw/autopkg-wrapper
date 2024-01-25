af2d813c05b2a7d3daa3a16aa02830dcbc958614
James Smith <james@smithjw.me>
2024-01-25 15:18:57 +1100

    Moving Slack notification into separate module

--- autopkg_wrapper/autopkg_wrapper.py
+++ autopkg_wrapper/autopkg_wrapper.py
@@ -1,17 +1,15 @@
-import json
 import os
 import plistlib
 import subprocess
 import sys
 from pathlib import Path
 
-import requests
 from github import Github
+from notifier import slack
 from utils.args import setup_args
 from utils.logging import setup_logger
 
-DEBUG = os.getenv("DEBUG", False)
-SLACK_WEBHOOK_TOKEN = os.getenv("SLACK_WEBHOOK_TOKEN", None)
+# DEBUG = os.getenv("DEBUG", False)
 AUTOPKG_TRUST_BRANCH = os.getenv("AUTOPKG_TRUST_BRANCH", None)
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", None)
 
@@ -246,67 +244,6 @@ def parse_recipe(recipe):
     return Recipe(recipe)
 
 
-def slack_alert(recipe):
-    if DEBUG:
-        print("Skippingk Slack notification as DEBUG is enabled!")
-        return
-
-    if SLACK_WEBHOOK_TOKEN is None:
-        print("Skipping Slack Notification as no SLACK_WEBHOOK_TOKEN defined!")
-        return
-
-    if recipe.verified is False:
-        task_title = f"{recipe.name} failed trust verification"
-        task_description = recipe.results["message"]
-    elif recipe.error:
-        task_title = f"Failed to import {recipe.name}"
-        if not recipe.results["failed"]:
-            task_description = "Unknown error"
-        else:
-            task_description = ("Error: {} \n" "Traceback: {} \n").format(
-                recipe.results["failed"][0]["message"],
-                recipe.results["failed"][0]["traceback"],
-            )
-
-            if "No releases found for repo" in task_description:
-                # Just no updates
-                return
-    elif recipe.updated:
-        task_title = f"{recipe.name} has been uploaded to Jamf"
-        task_description = f"It's time to test {recipe.name}!"
-    else:
-        # Also no updates
-        return
-
-    response = requests.post(
-        SLACK_WEBHOOK_TOKEN,
-        data=json.dumps(
-            {
-                "attachments": [
-                    {
-                        "username": "Autopkg",
-                        "as_user": True,
-                        "title": task_title,
-                        "color": "warning"
-                        if not recipe.verified
-                        else "good"
-                        if not recipe.error
-                        else "danger",
-                        "text": task_description,
-                        "mrkdwn_in": ["text"],
-                    }
-                ]
-            }
-        ),
-        headers={"Content-Type": "application/json"},
-    )
-    if response.status_code != 200:
-        raise ValueError(
-            "Request to slack returned an error %s, the response is:\n%s"
-            % (response.status_code, response.text)
-        )
-
-
 def main():
     recipe = AUTOPKG_RECIPES
     if DEBUG:
@@ -364,8 +301,9 @@ def main():
                     recipe, repo_url, remote_repo_ref, branch_name
                 )
 
-    # Send Slack Notification stating success of run or link to Pull Request
-    # slack_alert(recipe)
+    # Send Slack Notification if Slack token is present
+    if args.slack_token:
+        slack.send_notification(recipe, args.slack_token)
 
 
 if __name__ == "__main__":
---
+++ autopkg_wrapper/notifier/slack.py
@@ -0,0 +1,63 @@
+import json
+import logging
+
+import requests
+
+
+def send_notification(recipe, slack_token):
+    logging.debug("Skipping Slack notification as DEBUG is enabled!")
+
+    if slack_token is None:
+        logging.error("Skipping Slack Notification as no SLACK_WEBHOOK_TOKEN defined!")
+        return
+
+    if recipe.verified is False:
+        task_title = f"{recipe.name} failed trust verification"
+        task_description = recipe.results["message"]
+    elif recipe.error:
+        task_title = f"Failed to import {recipe.name}"
+        if not recipe.results["failed"]:
+            task_description = "Unknown error"
+        else:
+            task_description = ("Error: {} \n" "Traceback: {} \n").format(
+                recipe.results["failed"][0]["message"],
+                recipe.results["failed"][0]["traceback"],
+            )
+
+            if "No releases found for repo" in task_description:
+                # Just no updates
+                return
+    elif recipe.updated:
+        task_title = f"{recipe.name} has been uploaded to Jamf"
+        task_description = f"It's time to test {recipe.name}!"
+    else:
+        # Also no updates
+        return
+
+    response = requests.post(
+        slack_token,
+        data=json.dumps(
+            {
+                "attachments": [
+                    {
+                        "username": "Autopkg",
+                        "as_user": True,
+                        "title": task_title,
+                        "color": "warning"
+                        if not recipe.verified
+                        else "good"
+                        if not recipe.error
+                        else "danger",
+                        "text": task_description,
+                        "mrkdwn_in": ["text"],
+                    }
+                ]
+            }
+        ),
+        headers={"Content-Type": "application/json"},
+    )
+    if response.status_code != 200:
+        raise ValueError(
+            "Request to slack returned an error %s, the response is:\n%s"
+            % (response.status_code, response.text)
+        )
